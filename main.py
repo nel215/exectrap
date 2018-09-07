08a982e2e26350f02214e94226eac4577cded50a
nel215 <otomo.yuhei@gmail.com>
2018-09-07 23:06:24 +0900

    Add python version

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import pty
+
+
+def stdin_read(fd):
+    data = os.read(fd, 1024)
+    if data == b'\x10':
+        data += b'\x00\x7f'
+    return data
+
+
+def main():
+    argv = sys.argv[1:]
+    pty.spawn(argv, stdin_read=stdin_read)
+
+
+if __name__ == '__main__':
+    main()
